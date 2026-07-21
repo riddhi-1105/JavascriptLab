a98ca01d7e103199ff8e746cc34866c48e2ced27
Riddhi Thakkar <riddhithakkar1105@gmail.com>
2026-07-21 09:45:18 +0530

    Add files via upload

---
+++ Prac 1/function.js
@@ -0,0 +1,15 @@
+function greet() {
+
+    alert("Welcome to SIT Nagpur!");
+
+    console.table([
+        {
+            Name: "Sample User",
+            Course: "JavaScript"
+        }
+    ]);
+
+    console.time("Execution");
+    console.timeEnd("Execution");
+
+}
